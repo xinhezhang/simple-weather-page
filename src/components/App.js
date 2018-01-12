dd4044b9a7ef24840684356f93df4e273c47dc3d
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 00:39:30 -0800

    fixed intent

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,13 @@ import '../style/style.css'
 
 import Header from './header';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-      </div>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Header />
+            </div>
+        );
+    }
 }
 
-export default App;

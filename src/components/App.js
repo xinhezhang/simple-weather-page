9686ebf0836c47e0408a39b0a9f4e272843745d0
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 12:28:51 -0800

    add App-intro on the page

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ export default class App extends Component {
         return (
             <div className="App">
                 <Header />
+                <h3 className="App-intro">Find five-day forecast in your city</h3>
                 <SearchBar />
                 <WeatherList />
             </div>

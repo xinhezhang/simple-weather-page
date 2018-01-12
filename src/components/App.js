7c788ed3077d5992ca53a6d36e3942709f87cf67
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 00:42:06 -0800

    create SearchBar container

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import '../style/style.css'
 
 import Header from './header';
+import SearchBar from '../containers/search_bar';
 
 export default class App extends Component {
     render() {
         return (
             <div className="App">
                 <Header />
+                <SearchBar />
             </div>
         );
     }

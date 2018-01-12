dca7fa691eec8a6dd53a2de106d31e8c3c3fbb71
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 01:45:01 -0800

    add comments

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from 'axios';      // https://www.npmjs.com/package/axios
 
 // api key for http://openweathermap.org/forecast5
 const API_KEY = '7114e5cb52efc6d8bcbc3935f2a12fb4';
@@ -8,7 +8,7 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city, country = 'us') {
     const url = `${ROOT_URL}&q=${city},${country}`;
-    const request = axios.get(url);
+    const request = axios.get(url);     // similar as jquery's AJAX
 
     return {
         type: FETCH_WEATHER,

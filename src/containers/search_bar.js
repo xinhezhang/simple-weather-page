9f506e68b49d1a4da72376dc979d38564fe9986a
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 02:31:46 -0800

    fixed error

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -10,7 +10,7 @@ class SearchBar extends Component {
             term: '',
         };
         this.onInputChange = this.onInputChange.bind(this);
-        this.onFormSubmit = this.onFormSubmit(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
     onInputChange(event) {
         this.setState({

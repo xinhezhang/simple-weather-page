afb9c0bbfc344c03863485da5e0fba19aa834657
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 03:05:13 -0800

    update state by create a new one

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,17 @@
-export default function (state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state = [], action) {
     //console.log('Action received ', action);
-    return state;
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // Using "concat" instead of "push/append" since it create a new array instead of edit original array,
+            // which should NEVER happen, since we do NOT change state directly (like using this.setState()).
+            // This rule applies for both Redux and React!!!
+            //return state.concat([ action.payload.data ]);
+            
+            // same as above, using spread operator
+            return [ action.payload.data, ...state ];
+        default:
+            return state;
+    }
 }

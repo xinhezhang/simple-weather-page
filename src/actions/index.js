a163edf50313cb983cd9859f6033f8bd5ac669f6
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 01:40:44 -0800

    generate API call URL

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,13 @@
 // api key for http://openweathermap.org/forecast5
 const API_KEY = '7114e5cb52efc6d8bcbc3935f2a12fb4';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city, country = 'us') {
+    const url = `${ROOT_URL}&q=${city},${country}`;
+    
+    return {
+        type: FETCH_WEATHER,
+    };
+}

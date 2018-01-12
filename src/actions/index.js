f4effa29ff17d6c0ac2f5bdd2f85cc7ad5c9764d
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 02:32:50 -0800

    add country in api call, set default to "us"

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,8 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather(city) {
-    const url = `${ROOT_URL}q=${city},us&appid=${API_KEY}`;
+export function fetchWeather(city, country = 'us') {
+    const url = `${ROOT_URL}q=${city},${country}&appid=${API_KEY}`;
     const request = axios.get(url);     // similar as jquery's AJAX
 
     return {

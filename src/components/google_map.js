6022a8d0bc2a19ebba8e95df477a98a3b4f085fe
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 13:58:15 -0800

    fixed error (google not defined) by using window.google

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default class GoogleMap extends Component {
     componentDidMount() {
+        const google = window.google;
         new google.maps.Map(this.refs.map, {
             zoom: 12,
             center: {

30b22320949c52a97b0644e914b549ed9a6b7bb2
xinhezhang <xinhezhang@hotmail.com>
2018-01-12 13:23:47 -0800

    convert units to metric

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,12 @@ import axios from 'axios';      // https://www.npmjs.com/package/axios
 // api key for http://openweathermap.org/forecast5
 const API_KEY = '7114e5cb52efc6d8bcbc3935f2a12fb4';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?`;
+const UNIT = 'metric';     // 'standard', 'metric', 'imperial'
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city, country = 'us') {
-    const url = `${ROOT_URL}q=${city},${country}&appid=${API_KEY}`;
+    const url = `${ROOT_URL}q=${city},${country}&units=${UNIT}&appid=${API_KEY}`;
     const request = axios.get(url);     // similar as jquery's AJAX, "request" is a Promise
 
     //console.log('Request: ', request);
